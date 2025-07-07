cd8fbf43ed8b406a34e7182c4ada4164a2326456
rimcodedev <114328959+rimcodedev@users.noreply.github.com>
2025-07-07 21:26:11 +0200

    Invoice App. Create a form to add items

--- ReactWithSpringBoot/4-invoice-app/src/InvoiceApp.jsx
+++ ReactWithSpringBoot/4-invoice-app/src/InvoiceApp.jsx
@@ -4,10 +4,17 @@ import { ClientView } from "./components/ClientView";
 import { CompanyView } from "./components/CompanyView";
 import { ListItemsViews } from "./components/ListItemsViews";
 import { TotalView } from "./components/TotalView";
+import { useState } from "react";
 
 export const InvoiceApp = () => {
 
-    const { id, name, client, company, items, total } = getInvoice();
+    const { id, name, client, company, items: itemsInitial, total } = getInvoice();
+
+    const [productValue, setProductValue] = useState("");
+    const [priceValue, setPriceValue] = useState(0);
+    const [quantityValue, setQuantityValue] = useState(0);
+
+    const [items, setItems] = useState(itemsInitial);
 
     return (
         <>
@@ -17,20 +24,49 @@ export const InvoiceApp = () => {
                         Invoice Example
                     </div>
                     <div className="card-body">
-                        <InvoiceView id={ id } name={ name } />
+                        <InvoiceView id={id} name={name} />
 
                         <div className="row my-3">
                             <div className="col">
-                                <ClientView title="Client data" client={ client } />
+                                <ClientView title="Client data" client={client} />
                             </div>
 
                             <div className="col">
-                                <CompanyView title="Company data" company={ company } />
+                                <CompanyView title="Company data" company={company} />
                             </div>
                         </div>
 
-                        <ListItemsViews title="Invoice Items" items={ items } />
-                        <TotalView total = { total } />
+                        <ListItemsViews title="Invoice Items" items={items} />
+                        <TotalView total={total} />
+
+                        <form className="w-50" onSubmit={event => {
+                                event.preventDefault();
+                                setItems([
+                                    ...items,
+                                    {
+                                        key: 4,
+                                        product: productValue,
+                                        price: priceValue,
+                                        quantity: quantityValue
+                                    }
+                                ]);
+                                }
+                            }>
+                            <input type="text" name="product" placeholder="Product" className="form-control m-3"
+                                onChange={event => { console.log(event.target.value);
+                                                    setProductValue(event.target.value);
+                                 }} />
+                            <input type="text" name="price" placeholder="Price" className="form-control m-3"
+                                onChange={event => { console.log(event.target.value);
+                                                    setPriceValue(event.target.value);
+
+                                }} />
+                            <input type="text" name="quantity" placeholder="Quantity" className="form-control m-3"
+                                onChange={event => { console.log(event.target.value);
+                                                    setQuantityValue(event.target.value);
+                                 }} />
+                            <button type="submit" className="btn btn-primary m-3">Add Item</button>
+                        </form>
                     </div>
                 </div>
             </div>
